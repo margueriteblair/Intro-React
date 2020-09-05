9b5588916ea3fb06e76fbd2399e753ce57bfc160
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-05 05:49:37 -0400

    created a simple JSX element and a complex and wrapped JSX element

---
+++ createJSXElement.jsx
@@ -0,0 +1,17 @@
+//simple JSX element
+const JSX = <h1>Hello JSX!</h1>;
+
+
+//CREATE A COMPLEX JSX ELEMENT 
+//1. One important thing to know about nested JSX is that it must return a single element
+//2. One parent element would wrap all of the levels of nested elemets
+const JSX = 
+<div>
+  <h1></h1>
+  <p></p>
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
