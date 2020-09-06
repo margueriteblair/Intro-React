8bd17ab6eb0c80e51e069ab39af0173a75c9d9a3
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-06 14:39:09 -0400

    passed a property to a class component

---
+++ classComponentProps.jsx
@@ -0,0 +1,30 @@
+class ReturnRempPassword extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <p>Your temporary password is <strong>{this.props.tempPassword}</strong></p>
+            </div>
+        )
+    }
+}
+
+class ResetPassword extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <h2>Reset Password</h2>
+                <h3>We've generated a new temporary password for you.</h3>
+                <h3>Please reset this password from your account settings ASAP.</h3>
+                <ReturnRempPassword tempPassword="helloworld234"/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(ResetPassword, targetNode);
