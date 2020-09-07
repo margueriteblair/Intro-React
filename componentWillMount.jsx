dd816b1d910eaa3178a90212826204dfa89a3774
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-07 11:05:17 -0400

    component will mount method called before the render method

---
+++ componentWillMount.jsx
@@ -0,0 +1,14 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // change code below this line
+      console.log('hello')
+      // change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
+  
