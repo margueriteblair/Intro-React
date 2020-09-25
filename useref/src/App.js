aa314364fd9a6df23c15ac4ae84f9d7651724dfa
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-25 18:18:52 -0400

    use ref hook to display a users name

--- useref/src/App.js
+++ useref/src/App.js
@@ -2,17 +2,23 @@ import React, {useEffect, useState, useRef} from 'react';
 
 function App() {
   const [name, setName] = useState('')
-  const renderCount = useRef(1)
+  const prevName = useRef('')
 
+  // function focus() {
+  //   console.log(inputRef.current.focus)
+  //   inputRef.current.value = 'Some Value'
+  // }
   useEffect(() => {
-    renderCount.current = renderCount.current + 1
-  })
+    prevName.current = name
+  }, [name])
+
 
   return (
     <div>
-      <input value={name} onChange={()=> setName(e.target.value)}/>
-      <div>My name is {name}</div>
-      <div>I rendered {renderCount.current}</div>
+      <input value={name} onChange={(e)=> setName(e.target.value)}/>
+  <div>My name is {name}, but it used to be {prevName.current}</div>
+      {/* <div>I rendered {renderCount.current}</div> */}
+      {/* <button onClick={focus}>Focus</button> */}
     </div>
   );
 }
