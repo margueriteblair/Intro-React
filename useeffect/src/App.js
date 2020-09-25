9108e952656f69b274bece2c1a69c337508d78d0
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-25 17:24:56 -0400

    added usee effect hook to run when resource type is changed

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const [resourceType, setResourceType] = useState('posts')
+
+  useEffect(() => {
+    console.log('render')
+  },[resourceType])
   return (
+  <>
     <div>
       <button onClick={() => { setResourceType('posts')}}>Posts</button>
-      <button onClick={() => { setResourceType('users')}}>Useers</button>
+      <button onClick={() => { setResourceType('users')}}>Users</button>
       <button onClick={() => { setResourceType('comments')}}>Comments</button>
     </div>
+    <h1>{resourceType}</h1>
+  </>
   );
 }
 
